4ed1d0c88c6efa7ef3687a3a3ad1bc641472878a
Jonathan Aceituno <join@joinBook.local>
2014-09-27 15:52:01 +0200

    Modified the vagrant file to run Ubuntu with file sharing, X11 forwarding and audio output, thus allowing to use the simulator

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,14 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "terrywang/archlinux"
+  config.vm.box = "hashicorp/precise32"
+  config.ssh.forward_x11 = true
   config.vm.provision :shell, path: "vagrant-bootstrap.sh"
+  config.vm.network :private_network, ip: '192.168.50.50'
+  config.vm.synced_folder '.', '/vagrant', nfs: true
+  config.vm.provider "virtualbox" do |v|
+	v.customize ["modifyvm", :id, "--cpuexecutioncap", "90"]
+	v.customize ["modifyvm", :id, "--cpus", 1]
+	v.customize ["modifyvm", :id, "--audio", "coreaudio", "--audiocontroller", "ac97"]
+  end
 end
--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Install the base tools
-pacman -Sy --noconfirm git base-devel texinfo zip libunistring
+apt-get -y update && apt-get -y install build-essential texinfo git automake libtool perl zip flex bison xserver-xorg-core libsdl1.2-dev 
+
+
 # Install the custom ARM toolchain
-cd /vagrant/tools && echo a | ./rockboxdev.sh
+mkdir -p build-rbdev && cd /vagrant/tools && echo a | RBDEV_BUILD=/vagrant/build-rbdev ./rockboxdev.sh #&& rm -rf ../build-rbdev
 # Configure Rockbox to perform a standard build
-cd /vagrant && mkdir build-vagrant && cd build-vagrant && ../tools/configure --target=65 --type=n
+cd /vagrant && mkdir -p build-vagrant && cd build-vagrant && ../tools/configure --target=65 --type=n
+
+# Install the ALSA sound driver
+usermod -a -G audio vagrant
+apt-get install -y alsa-base alsa-utils libasound2-plugins
+
+# Configure sshd for X11 forwarding
+if [[ ! -f /.sshd-x11-forward-done ]]; then
+	echo "X11Forwarding yes" >>/etc/ssh/sshd_config
+	echo "X11UseLocalhost no" >>/etc/ssh/sshd_config
+	touch /.sshd-x11-forward-done
+	echo "The virtual machine will now reboot to enable X11 forwarding."
+	reboot
+fi
